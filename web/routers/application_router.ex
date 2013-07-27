88581bfa17f836ab3c7de01b1bfbd49c5a01f15d
Johnny Winn <dev+nurugger07@hashrocket.com>
2013-07-27 09:40:49 -0400

    Require posts and repo in application router

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -1,35 +1,5 @@
-alias Ecto.Adapters.Postgres
-
-defmodule MyRepo do
-  use Ecto.Repo, adapter: Postgres
-
-  def url, do: "ecto://blogger@localhost/blog"
-end
-
-defmodule Post do
-  use Ecto.Entity
-
-  dataset :posts do
-    field :headline, :string
-    field :content, :string
-  end
-end
-
-defmodule PostQueries do
-  import Ecto.Query
-
-  def find_by_id(id) do
-    { :ok, post } = Enum.fetch(_find_by_id(id), 0)
-    post
-  end
-
-  def all, do: Enum.map(_all, fn(x) -> {x.headline, x.content} end)
-
-  defp _all, do: MyRepo.all(from p in Post)
-  defp _find_by_id(id), do: MyRepo.all(from p in Post, where: p.id == id)
-end
-
-{ :ok, _pid } = Postgres.start(MyRepo)
+Code.require_file "../repo.ex", __FILE__
+Code.require_file "../posts.ex", __FILE__
 
 defmodule ApplicationRouter do
   use Dynamo.Router
