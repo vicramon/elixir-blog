6454187fc597a3a8dd8d3bac978bb83ab84c62f5
Johnny Winn <dev+nurugger07@hashrocket.com>
2013-07-28 11:02:39 -0400

    Refactor post entity queries

--- web/routers/posts.ex
+++ web/routers/posts.ex
@@ -11,12 +11,15 @@ defmodule PostQueries do
   import Ecto.Query
 
   def find_by_id(id) do
-    { :ok, post } = Enum.fetch(_find_by_id(id), 0)
-    post
+    _find_by_id(id) |> post
   end
+
+  def post({ :ok, post }),  do: post
+  def post(:error), do: nil
 
   def all, do: Enum.map(_all, fn(x) -> {x.headline, x.content} end)
 
   defp _all, do: MyRepo.all(from p in Post)
-  defp _find_by_id(id), do: MyRepo.all(from p in Post, where: p.id == id)
+
+  defp _find_by_id(id), do: MyRepo.all(from p in Post, where: p.id == id) |> Enum.fetch(0)
 end
--- web/routers/posts_router.ex
+++ web/routers/posts_router.ex
@@ -11,6 +11,11 @@ defmodule PostsRouter do
 
   get "/:post_id" do
     { id, _ } = String.to_integer(post_id)
-    render conn, "post.html", post: PostQueries.find_by_id(id)
+    render_post(conn, PostQueries.find_by_id(id))
   end
+
+  def render_post(conn, post) when post == nil, do: render(conn, "404.html")
+
+  def render_post(conn, post), do: render(conn, "post.html", post: post)
+
 end
